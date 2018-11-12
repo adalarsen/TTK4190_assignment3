f2e8f3c7e4c176df73299796e42c3cc7d5a16934
Unknown <Adasl@stud.ntnu.no>
2018-11-12 14:23:46 +0100

    Add 1.8 speed controller

--- exKT.m
+++ exKT.m
@@ -9,25 +9,25 @@ N = 2000;  % number of samples
 h = 0.1;   % sample time
 
 
-delta_R = 5*(pi/180);      % rudder angle step input
+delta_R = 7*(pi/180);      % rudder angle step input
 
 x=[p psi v r];
 
 % time-series
 tdata = tout;
-rdata = r(1:length(r)-1)*180/pi;   
+udata = v(1:length(r)-1,1);%*180/pi;   
 
 % nonlinear least-squares parametrization: T dr/dt + r = K delta,   delta = -delta_R
 % x(1) = 1/T and x(2) = K
 x0 = [0.1 1]'
 F = inline('exp(-tdata*x(1))*0 + x(2)*(1-exp(-tdata*x(1)))*5','x','tdata')
-x = lsqcurvefit(F,x0, tdata, rdata);
+x = lsqcurvefit(F,x0, tdata, udata);
 
 % estimated parameters
 T = 1/x(1)
 K = x(2)
 
 figure(gcf)
-plot(tdata,rdata,'g',tdata,exp(-tdata*x(1))*0 + x(2)*(1-exp(-tdata*x(1)))*5,'r'),grid
+plot(tdata,udata,'g',tdata,exp(-tdata*x(1))*0 + x(2)*(1-exp(-tdata*x(1)))*5,'r'),grid
 title('Nonlinear least-squares fit of Nomoto model for \delta = 5 (deg)'),xlabel('time (s)')
 legend('Nonlinear model','Estimated 1st-order Nomoto model')
